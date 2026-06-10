Dustin4444/protobuf-1

Fix Pure Python `map<string, ...>` key handling for `bytes` lookups.
The pure Python implementation of maps had a bug where looking up an entry in a `map<string, ValueType>` field using a `bytes` key would cause silent data corruption.

**Bug:**

1.  `msg.my_map['foo'] = 100` stores `{'foo': 100}`.
2.  `msg.my_map[b'foo']` attempts a lookup.
3.  `self._values[b'foo']` raises `KeyError` as `b'foo' != 'foo'`.
4.  The `except KeyError` block normalizes `b'foo'` to `'foo'`.
5.  Crucially, it then inserts a *default value* for the value type, overwriting the existing entry: `self._values['foo'] = 0`.
6.  The lookup returns 0, and the original value of 100 is lost.

**Fix:**

The key is now normalized using `self._key_checker.CheckValue(key)` *at the beginning* of `__getitem__`, `__contains__`, `get`, `__delitem__`, and `setdefault`. This ensures the key is in the canonical `str` format *before* any dictionary access, preventing the erroneous write-on-miss.

This change makes the behavior consistent with the C++ and upb implementations.

PiperOrigin-RevId: 926141533

python/google/protobuf/internal/descriptor_test.py
@@ -412,6 +412,59 @@ class DescriptorTest(unittest.TestCase):
     # Modification is (unfortunately) reflected in the descriptor.
     self.assertTrue(self.my_message.GetOptions().deprecated)
 
+  def testImmutableMapLookup(self):
+    complex_opt1 = unittest_custom_options_pb2.complex_opt1
+    msg_proto = descriptor_pb2.DescriptorProto()
+    msg_proto.name = 'TestMessage_%d' % id(self)
+    # Set a map value in the options.
+    # ComplexOptionType1 has a field 'my_map' of type map<string, int32>.
+    opt = msg_proto.options.Extensions[complex_opt1]
+    opt.my_map['key'] = 123
+    opt.my_map['other_key'] = 456
+
+    desc = descriptor.MakeDescriptor(msg_proto)
+    options = desc.GetOptions()
+    immutable_map = options.Extensions[complex_opt1].my_map
+
+    # Test lookups.
+    self.assertEqual(immutable_map['key'], 123)
+    self.assertEqual(immutable_map['other_key'], 456)
+    self.assertIn('key', immutable_map)
+    self.assertIn('other_key', immutable_map)
+    self.assertNotIn('nonexistent_key', immutable_map)
+    self.assertEqual(len(immutable_map), 2)
+
+    # Test lookups via bytes.
+    self.assertEqual(immutable_map[b'key'], 123)
+    self.assertEqual(immutable_map[b'other_key'], 456)
+    self.assertIn(b'key', immutable_map)
+    self.assertIn(b'other_key', immutable_map)
+    self.assertNotIn(b'nonexistent_key', immutable_map)
+    self.assertEqual(len(immutable_map), 2)
+
+    # Test iteration.
+    self.assertEqual(set(immutable_map.keys()), {'key', 'other_key'})
+    self.assertEqual(set(immutable_map.values()), {123, 456})
+    self.assertEqual(
+        set(immutable_map.items()), {('key', 123), ('other_key', 456)}
+    )
+
+    # Test get().
+    self.assertEqual(immutable_map.get('key'), 123)
+    self.assertEqual(immutable_map.get('nonexistent_key'), None)
+    self.assertEqual(immutable_map.get('nonexistent_key', 999), 999)
+
+    # Test get() with bytes.
+    self.assertEqual(immutable_map.get(b'key'), 123)
+    self.assertEqual(immutable_map.get(b'nonexistent_key'), None)
+    self.assertEqual(immutable_map.get(b'nonexistent_key', 999), 999)
+
+    # Test immutability
+    if api_implementation.Type() != 'cpp':
+      immutability_error = TypeError
+      with self.assertRaises(immutability_error):
+        immutable_map['new_key'] = 789
+
   def testSimpleCustomOptions(self):
     file_descriptor = unittest_custom_options_pb2.DESCRIPTOR
     message_descriptor = (

CodeLinaro-mirror/le_aisw_ai-orchestrator_protobuf

python/google/protobuf/internal/descriptor_test.py
@@ -337,12 +337,16 @@ class DescriptorTest(unittest.TestCase):
 
     # Unset submessage mutation
     complex_opt1 = unittest_custom_options_pb2.complex_opt1
-    stub_submsg = unittest_pb2.TestAllTypes.DESCRIPTOR.GetOptions().Extensions[complex_opt1]
+    stub_submsg = unittest_pb2.TestAllTypes.DESCRIPTOR.GetOptions().Extensions[
+        complex_opt1
+    ]
     with self.assertRaises(immutability_error):
       stub_submsg.foo = 5
 
     # Non-empty repeated field mutation
-    complex_options_msg = unittest_custom_options_pb2.VariousComplexOptions.DESCRIPTOR.GetOptions()
+    complex_options_msg = (
+        unittest_custom_options_pb2.VariousComplexOptions.DESCRIPTOR.GetOptions()
+    )
     non_empty_repeated = complex_options_msg.Extensions[complex_opt1].foo4
     self.assertEqual(len(non_empty_repeated), 2)
     with self.assertRaises(immutability_error):
@@ -411,6 +415,41 @@ class DescriptorTest(unittest.TestCase):
 
     # Modification is (unfortunately) reflected in the descriptor.
     self.assertTrue(self.my_message.GetOptions().deprecated)
+
+  def testImmutableMapLookup(self):
+    complex_opt1 = unittest_custom_options_pb2.complex_opt1
+    complex_options_msg = (
+        unittest_custom_options_pb2.VariousComplexOptions.DESCRIPTOR.GetOptions()
+    )
+    immutable_map = complex_options_msg.Extensions[complex_opt1].my_map
+
+    # Test lookups.
+    self.assertEqual(immutable_map['key'], 123)
+    self.assertEqual(immutable_map['other_key'], 456)
+    self.assertIn('key', immutable_map)
+    self.assertIn('other_key', immutable_map)
+    self.assertNotIn('nonexistent_key', immutable_map)
+    self.assertEqual(len(immutable_map), 2)
+
+    # Test lookups via bytes.
+    self.assertEqual(immutable_map[b'key'], 123)
+    self.assertEqual(immutable_map[b'other_key'], 456)
+    self.assertIn(b'key', immutable_map)
+    self.assertIn(b'other_key', immutable_map)
+    self.assertNotIn(b'nonexistent_key', immutable_map)
+    self.assertEqual(len(immutable_map), 2)
+
+    # Test iteration.
+    self.assertEqual(set(immutable_map.keys()), {'key', 'other_key'})
+    self.assertEqual(set(immutable_map.values()), {123, 456})
+    self.assertEqual(
+        set(immutable_map.items()), {('key', 123), ('other_key', 456)}
+    )
+
+    # # Test get().
+    self.assertEqual(immutable_map.get('key'), 123)
+    self.assertEqual(immutable_map.get('nonexistent_key'), None)
+    self.assertEqual(immutable_map.get('nonexistent_key', 999), 999)
 
   def testSimpleCustomOptions(self):
     file_descriptor = unittest_custom_options_pb2.DESCRIPTOR