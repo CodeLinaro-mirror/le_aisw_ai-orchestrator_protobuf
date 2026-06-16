934180fdff204674af9b7eab4666a1a6c8885c11
Clayton Knittel <cknittel@google.com>
2026-06-16 15:43:59 -0700

    [custom vtable] Access `MessageLite::_class_data_` through an inline accessor.
    This is for profiling purposes.
    
    PiperOrigin-RevId: 933339242

--- src/google/protobuf/compiler/cpp/field_generators/enum_field.cc
+++ src/google/protobuf/compiler/cpp/field_generators/enum_field.cc
@@ -324,7 +324,7 @@ class RepeatedEnum : public FieldGeneratorBase {
     p->Emit({InternalMetadataOffsetSub(p)},
             R"cc(
               $name$_ {
-                visibility, $internal_metadata_offset$, from.$name$_
+                visibility, $internal_metadata_offset$, arena, from.$name$_
               }
             )cc");
   }
