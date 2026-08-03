92188b4852372cedcc311221f80feb068366478e
Vitaly Goldshteyn <goldvitaly@google.com>
2026-08-02 18:12:03 -0700

    Change ExtensionSet flat capacity growth factor from 4x to 2x.
    This reduces memory overhead when resizing the flat array in ExtensionSet by growing the capacity by a factor of 2 instead of 4.
    
    PiperOrigin-RevId: 958086465

--- src/google/protobuf/extension_set.cc
+++ src/google/protobuf/extension_set.cc
@@ -1611,7 +1611,7 @@ void ExtensionSet::GrowCapacity(Arena* arena, size_t minimum_new_capacity) {
 
   auto new_flat_capacity = flat_capacity_;
   do {
-    new_flat_capacity = new_flat_capacity == 0 ? 1 : new_flat_capacity * 4;
+    new_flat_capacity = new_flat_capacity == 0 ? 1 : new_flat_capacity * 2;
   } while (new_flat_capacity < minimum_new_capacity);
 
   KeyValue* begin = flat_begin();
