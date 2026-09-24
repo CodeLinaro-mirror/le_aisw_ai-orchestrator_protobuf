c4590fff1a74146fc85aa6c861777d4ed8544212
Samuel Freilich <sfreilich@google.com>
2026-09-24 12:07:18 -0700

    Avoid EncodeError when ByteSize is called on message missing required fields
    PiperOrigin-RevId: 987666367

--- python/message.c
+++ python/message.c
@@ -1635,7 +1635,11 @@ static PyObject* PyUpb_Message_ByteSize(PyObject* self, PyObject* args) {
   // moment upb does not have a "byte size" function, so we just serialize to
   // string and get the size of the string.
   PyObject* subargs = PyTuple_New(0);
-  PyObject* serialized = PyUpb_Message_SerializeToString(self, subargs, NULL);
+  // SerializePartialToString because according to the documentation for
+  // Message.ByteSize, this is not expected to raise EncodeError if required
+  // fields are not set.
+  PyObject* serialized =
+      PyUpb_Message_SerializePartialToString(self, subargs, NULL);
   Py_DECREF(subargs);
   if (!serialized) return NULL;
   size_t size = PyBytes_Size(serialized);
